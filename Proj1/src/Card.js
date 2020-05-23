717a20eacdbbda734808d4ae9eb5e9e222f20b90
meetgadoya <gadoya@usc.edu>
2020-05-23 12:07:22 -0700

    minor change

--- Proj1/src/Card.js
+++ Proj1/src/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 // function Card() {        either this line or below one
-const Card = ({email, id, name}) => {
+const Card = (props/*{email, id, name}*/) => {
     
     return(
         <div className = 'tc ba bg-light-green dib ma3 pa2 br3 grow bw2 shadow-5' >
-            <img src = {`https://robohash.org/${id}?size=200x200`} alt='robots'/>
+            <img src = {`https://robohash.org/${props.id}?size=200x200`} alt='robots'/>
             <div>
-                <h3> {name}</h3>
-                <p> {email}</p>
+                <h3> {props.name}</h3>
+                <p> {props.email}</p>
             </div>
         </div>
     );
