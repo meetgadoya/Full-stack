b34f9f7f3bfd5139ae6a3d51843312ab2b8f670c
meetgadoya <gadoya@usc.edu>
2020-06-01 08:32:35 -0700

    added ComponentDidMount()

--- Proj4/src/App.js
+++ Proj4/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import CardList from './CardList.js';
 import SearchBox from './SearchBox.js';
-import {robots} from './robots.js';
 import './App.css';
 
 
@@ -9,13 +8,20 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots : robots,
+            robots : [],
             searchfield : ''
         }
     }
 
+    componentDidMount(){
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => {return response.json()})
+            .then(users => {this.setState({robots:users})})
+        // this.setState({robots : robots});
+    }
+
     onSearchChange = (event) => {
-        this.setState({searchfield : event.target.value})
+        this.setState({searchfield : event.target.value});
     }
 
     render(){
@@ -24,21 +30,29 @@ class App extends React.Component {
                 return rob.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
             }
         )
-        return(
-            <div className = "tc"> 
-                <h1 className = 'f1'> ROBOFRIENDS </h1>
+        
+        if(this.state.robots.length === 0){
+            return(<h1>Loading</h1>);
+        }
+        else {
+            return(
+                <div className = "tc"> 
+                    <h1 className = 'f1'> ROBOFRIENDS </h1>
 
-                {/*  EVEN FOLLOWING CODE WORKS INSTEAD OF HAVING SEARCHBOX 
-                <input className = "pa3 ba b--green bg-lightest-blue"
-            type = 'search' placeholder = 'search robots'
-            onChange = {this.onSearchChange}/>
-                 */}
-                <SearchBox searchchange = {this.onSearchChange}/>
-                <br/>
-                <CardList robot_list = {filteredRobots}/>
-            </div>
-        );
+                    {/*  EVEN FOLLOWING CODE WORKS INSTEAD OF HAVING SEARCHBOX 
+                    <input className = "pa3 ba b--green bg-lightest-blue"
+                type = 'search' placeholder = 'search robots'
+                onChange = {this.onSearchChange}/>
+                    */}
+                    <SearchBox searchchange = {this.onSearchChange}/>
+                    <br/>
+                    <CardList robot_list = {filteredRobots}/>
+                </div>
+            );
+        }
     }
+
+    
 }
 
 export default App;
